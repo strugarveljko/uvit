059b14b22f1d515f2bc7e2c706213579d1213f4f
strugarveljko <102263525+strugarveljko@users.noreply.github.com>
2023-05-17 15:37:39 +0200

    Add files via upload

---
+++ 2.js
@@ -0,0 +1,17 @@
+const slika = document.getElementById('slika');
+slika.addEventListener('mouseenter', uvecajSliku);
+slika.addEventListener('mouseleave', umanjiSliku);
+
+function uvecajSliku(){
+  slika.style.height = 'initial';
+}
+function umanjiSliku(){
+  slika.style.height = '150px';
+}
+
+const tr = document.getElementsByTagName('tr');
+for (const i in tr) {
+  tr[i].addEventListener('click', function(){
+    tr[i].style.display = 'none';
+  });
+}
